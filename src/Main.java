80d2efe2cdbaa74b9b7419594fa81fcd8812759f
Pavel <pavelkorolko@gmail.com>
2023-12-27 14:47:30 +0200

    added second and third tasks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scn = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        //Написать программу для вывода названия поры года по номеру месяца
+
+        //Uncomment each of task for checking
+
+        //1. Написать программу для вывода названия поры года по номеру месяца
+        //firstTask();
+
+        //2. Написать программу для вывода названия поры года по номеру месяца
+        //secondTask();
+
+        //3. Напишите программу, которая будет принимать на вход число и на выход будет выводить сообщение
+        //четное число или нет.
+        //thirdTask();
+
+        //4.
+
+    }
+
+    public static void firstTask() {
         System.out.print("Enter the month by a number: ");
         StringBuilder str = new StringBuilder("---> ");
 
@@ -24,6 +42,47 @@ public class Main {
                 default:
                     str.append("Input is out of range: 1-12");
                     break;
+            }
+        } else {
+            str.append("Wrong input!");
+        }
+        System.out.print(str);
+    }
+
+    public static void secondTask() {
+        System.out.print("Enter the month by a number: ");
+        StringBuilder str = new StringBuilder("---> ");
+
+        if (scn.hasNextInt()) {
+            int result = scn.nextInt();
+
+            if (result >= 1 && result < 3 || result == 12) {
+                str.append("Winter");
+            } else if (result >= 3 && result < 6) {
+                str.append("Spring");
+            } else if (result >= 6 && result < 9) {
+                str.append("Summer");
+            } else if (result >= 9 && result < 12) {
+                str.append("Autumn");
+            } else {
+                str.append("Input is out of range: 1-12");
+            }
+        } else {
+            str.append("Wrong input!");
+        }
+        System.out.print(str);
+    }
+
+    public static void thirdTask() {
+        System.out.print("Enter a number: ");
+        StringBuilder str = new StringBuilder("---> ");
+
+        if (scn.hasNextInt()) {
+            int result = scn.nextInt();
+            if (result % 2 == 0) {
+                str.append("Even number!");
+            } else {
+                str.append("Odd number!");
             }
         } else {
             str.append("Wrong input!");
